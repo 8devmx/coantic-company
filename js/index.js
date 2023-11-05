46cde7793935130025d881818182f62071a30c32
Abraham Pech <abraham.opp@gmail.com>
2023-11-05 18:21:01 -0500

    maquetado de home

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.paila_slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  })
+})
