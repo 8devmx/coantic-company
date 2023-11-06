084e73c109f2280f8fdbbd07d243ac3409b9b310
Abraham Pech <abraham.opp@gmail.com>
2023-11-05 21:53:27 -0500

    Maquetado de interna de productos

---
+++ js/productos-interna.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.slide').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    SlidesToScroll: 1
+  })
+})
